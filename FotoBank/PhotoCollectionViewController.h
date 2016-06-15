47516b15b648717706b3d5d07ef0195089f97a0d
Srinivas Bodhanampati <sbodhan@Srinivass-MacBook-Pro.local>
2016-06-15 10:10:36 -0400

    Created storyboard with collection view controller and detail VC

---
+++ FotoBank/PhotoCollectionViewController.h
@@ -0,0 +1,13 @@
+//
+//  PhotoCollectionViewController.h
+//  FotoBank
+//
+//  Created by Srinivas Bodhanampati on 6/15/16.
+//  Copyright © 2016 Srinivas Bodhanampati. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PhotoCollectionViewController : UICollectionViewController
+
+@end
--- FotoBank/PhotoDetailViewController.h
+++ FotoBank/PhotoDetailViewController.h
@@ -1,5 +1,5 @@
 //
-//  ViewController.h
+//  PhotoDetailViewController.h
 //  FotoBank
 //
 //  Created by Srinivas Bodhanampati on 6/15/16.
@@ -8,8 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
-
+@interface PhotoDetailViewController : UIViewController
 
 @end
-
